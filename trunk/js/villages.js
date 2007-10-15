3c40b52bc0fb14b1f76930071c8267b4f77f61d7
pipsqueaky <pipsqueaky@d6ccc596-253c-0410-a1c6-b3718876d141>
2007-10-15 21:57:34 +0000

    Added sorting for volunteers within each village

--- trunk/js/villages.js
+++ trunk/js/villages.js
@@ -53,6 +53,15 @@ function handleError(e)
 	}
 };
 
+function refreshVillageItinerary(selectedDate) {
+	/* set global variables beginSelectedDate and endSelectedDate - need them when examining the feed */
+	beginSelectedDate = new Date(selectedDate);
+	endSelectedDate = new Date(selectedDate);
+	endSelectedDate.setDate(endSelectedDate.getDate() + 1);
+	
+	myService.getAllCalendarsFeed(FEED, handleAllCalendarsForVillages, handleError);
+};
+
 function handleAllCalendarsForVillages(feedRoot)
 {
 	/* reset village itinerary info on page */
@@ -69,33 +78,16 @@ function handleAllCalendarsForVillages(feedRoot)
 		villageElement.setAttribute('id', calendarTitle);
 		villageElement.appendChild(createElementWithText('u', calendarTitle));
 		villageElement.appendChild(document.createElement('br'));
-		addSortedNodeToElement(document.getElementById('villageItinerary'), villageElement);
+		addSortedNodeToElement(document.getElementById('villageItinerary'), villageElement, true);
 
 		myService.getEventsFeed(calendar.getLink().getHref(), handleVillageItinerary, handleError);
 	}
-};
-
-function dummyFunction(feedRoot) {};
-
-function addSortedNodeToElement(element, childNodeToInsert)
-{
-	if (element.hasChildNodes() == true) {
-		for (i = 0; i < element.childNodes.length; i++) {
-			var childNode = element.childNodes[i];
-			if (childNodeToInsert.getAttribute('id') < childNode.getAttribute('id')) {
-				element.insertBefore(childNodeToInsert, childNode);
-				return;
-			}
-		}
-	}
-	element.appendChild(childNodeToInsert);
 };
 
 function handleVillageItinerary(feedRoot)
 {
 	var calendarId = feedRoot.feed.getTitle().getText();
 	var entries = feedRoot.feed.getEntries();
-	var foundMatchingEntries = false;
 	var countMatchingEntries = 0;
 	
 	var entryListElement = document.createElement('ul');
@@ -107,8 +99,7 @@ function handleVillageItinerary(feedRoot)
 			var entryStartDate = times[0].getStartTime().getDate();
 			var entryEndDate = times[0].getEndTime().getDate();
 			if (entryStartDate < endSelectedDate && entryEndDate > beginSelectedDate) {
-				foundMatchingEntries = true;
-				entryListElement.appendChild(createElementWithText('li', entry.getTitle().getText()));
+				addSortedNodeToElement(entryListElement, createElementWithText('li', entry.getTitle().getText()), false);
 				countMatchingEntries++;
 			}
 		}
@@ -125,13 +116,25 @@ function handleVillageItinerary(feedRoot)
 	document.getElementById(calendarId).appendChild(entryListElement);
 };
 
-function refreshVillageItinerary(selectedDate) {
-	/* set global variables beginSelectedDate and endSelectedDate - need them when examining the feed */
-	beginSelectedDate = new Date(selectedDate);
-	endSelectedDate = new Date(selectedDate);
-	endSelectedDate.setDate(endSelectedDate.getDate() + 1);
-	
-	myService.getAllCalendarsFeed(FEED, handleAllCalendarsForVillages, handleError);
+
+/**********************************************************/
+/* Utility functions                                      */
+/**********************************************************/
+
+function addSortedNodeToElement(element, childNodeToInsert, doSortById)
+{
+	if (element.hasChildNodes() == true) {
+		var newId = (doSortById == true) ? childNodeToInsert.getAttribute('id') : childNodeToInsert.innerHTML;
+		for (i = 0; i < element.childNodes.length; i++) {
+			var childNode = element.childNodes[i];
+			var currentId = (doSortById == true) ? childNode.getAttribute('id') : childNode.innerHTML;
+			if (newId < currentId) {
+				element.insertBefore(childNodeToInsert, childNode);
+				return;
+			}
+		}
+	}
+	element.appendChild(childNodeToInsert);
 };
 
 function createElementWithText(elementType, input)
